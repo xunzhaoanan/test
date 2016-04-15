3e10e26034abe8ee9f1a2259a7c099193dfc2394
xunzhaoanan <47331935@qq.com>
2016-04-15 22:06:17 +0800

    2233

---
+++ array_test.php
@@ -0,0 +1,19 @@
+<?php
+$array1 = range(1, 10);
+$array2 = array_reverse($array1);
+print_r($array2);
+echo "<br>";
+//shuffle($array2);
+/*
+while(list($a,$b) = each($array2))
+	echo $a."--".$b."<br>";
+*/
+$a = next($array2);
+echo $a;
+$b = next($array2);
+echo $b;
+$c = current($array2);
+echo $c;
+$d = reset($array2);
+echo $d;
+//print_r($array2);
